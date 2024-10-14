5e1fdd66e0317cacf18eca4da253b8de425abd01
James Wadley <86320001+w4dd325@users.noreply.github.com>
2024-10-14 11:27:35 +0100

    basic setup
    basic cypress schema validation test - for use with PR automation checks.

---
+++ tests/cypress/e2e/chcuk-norris-pass-spec.cy.js
@@ -0,0 +1,11 @@
+const schema = require('../fixtures/chuckNorrisSchemaPass.json');
+
+describe('API Schema Validation with Plain JSON', () => {
+  it('should validate the user data using plain JSON schema', () => {
+    cy.request('GET', 'https://api.chucknorris.io/jokes/random')
+      .validateSchema(schema)
+      .then(response => {
+        // Further assertions
+      });
+  });
+});
